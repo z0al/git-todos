5e48ae852a72525f980be9168a3169ff0ac34c90
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-12-07 21:01:47 +0200

    chore: initial commit

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+// Copyright © 2017 Ahmed T. Ali <ah.tajelsir@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+// Native
+import "fmt"
+import "os"
+
+// Packages
+import "github.com/spf13/cobra"
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:     "git-todos [command]",
+	Short:   "A Git-based Todos App for Developers",
+	Version: "v0.1.0",
+}
+
+// Execute adds all child commands to the root command and sets flags
+// appropriately. This is called by main.main().
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
